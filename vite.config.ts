f66e10a7d712d5f4164d7ea531d4b8bf3901df0b
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-08 11:12:34 +0800

    Add Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"), // Resolve path using __dirname
     },
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
